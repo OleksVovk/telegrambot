e098bf89b0d876519834011b8ca8de6b929e1440
OleksVovk <oleks.vovk@gmail.com>
2019-02-12 02:35:25 +0100

    added replying function

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-import requests
+import telebot
+from telebot.types import Message
 
-url = "https://api.telegram.org/<your_token>/"
-
-
-def get_updates(request):
-    response = requests.get(request + 'getUpdates')
-    return response.json()
+token = 'TOKEN'
+bot = telebot.TeleBot(token)
 
 
-def last_update(data):
-    results = data['result']
-    all_updates = len(results) - 1
-    return results[all_updates]
+@bot.message_handler(func=lambda message: True)
+def bold(message: Message):
+    bot.reply_to(message, message.text.upper())
+
+
+bot.polling()
+
+# url = "https://api.telegram.org/botTOKEN/"
+#
+# a = requests.get(f'{url}getMe')  # getMe - keyword
+# print(a.json())
+# b = requests.get(f'{url}getUpdates')
+# pprint.pprint(b.json())
+#
+# load = {}
+# load['chat_id'] = 3414751
+# load['latitude'] = 50.7472
+# load['longitude'] = 25.3254
+# c = requests.post(f'{url}sendLocation', data=load)
+
+
+
 
